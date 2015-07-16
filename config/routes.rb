097aba54c5e7a810175dbc49e1c8a7ebb5530117
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-16 18:49:37 +0900

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/new'
+  root to: 'welcome#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
