36eea2feff780b9dc1b90c560d89684f60af1124
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-18 06:06:27 +0900

    add create event form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :events
+
   root to: 'welcome#new'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
---
+++ spec/factories/events.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :event do
+    owner_id 1
+name "MyString"
+place "MyString"
+start_time "2015-07-18 04:51:49"
+end_time "2015-07-18 04:51:49"
+content "MyText"
+  end
+
+end
