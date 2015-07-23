e4cbe1bad0ffb7d2be2f5d5fd440222030007330
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-23 23:57:33 +0900

    add event test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    context 'nilのとき' do
+      let(:event) { Event.new(name: nil) }
+
+      it 'validでないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
+    context '空白のとき' do
+      let(:event) { Event.new(name: '') }
+
+      it 'validでないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
+    context 'Rails勉強会のとき' do
+      let(:event) { Event.new(name: 'Rails勉強会') }
+
+      it 'validであること' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context '50 文字のとき' do
+      let(:event) { Event.new(name: 'a' * 50) }
+
+      it 'validであること' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context '51 文字のとき' do
+      let(:event) { Event.new(name: 'a' * 51) }
+
+      it 'validでないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
+
 end
