aebb8651739e74e27437fef2e8a6589eaa8385d7
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-25 09:27:26 +0900

    view spec

---
+++ spec/views/events/show.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "events/show", type: :view do
+  context '未ログインユーザがアクセスしたとき' do
+    before do
+      allow(view).to receive(:logged_in?) { false }
+      allow(view).to receive(:current_user) { nil }
+    end
+
+    context 'かつ @event.owner が nil のとき' do
+      before do
+        assign(:event, create(:event, owner: nil))
+        assign(:tickets, [])
+      end
+
+      it '"退会したユーザです"とひょうじされていること' do
+        render
+        expect(rendered).to match /退会したユーザです/
+      end
+    end
+  end
+end
